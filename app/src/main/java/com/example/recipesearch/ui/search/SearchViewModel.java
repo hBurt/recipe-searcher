8662b40d0d8c4f2f334f2cae7588addc6f308113
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2019-12-05 16:14:07 -0500

    search screen and relevant classes

---
+++ app/src/main/java/com/example/recipesearch/ui/search/SearchViewModel.java
@@ -0,0 +1,21 @@
+package com.example.recipesearch.ui.search;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+
+public class SearchViewModel extends ViewModel
+{
+    private MutableLiveData<String> mText;
+
+    public SearchViewModel()
+    {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Recipe fragment");
+    }
+    public LiveData<String> getText()
+    {
+        return mText;
+    }
+}
