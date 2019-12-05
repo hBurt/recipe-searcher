65e6784529d5863be64b376913b8145a53f1b19d
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2019-12-05 17:12:30 -0500

    just more additions to the searchResultsActivity file

--- app/src/main/java/com/example/recipesearch/ui/SearchResultsActivity.java
+++ app/src/main/java/com/example/recipesearch/ui/SearchResultsActivity.java
@@ -2,8 +2,14 @@ package com.example.recipesearch.ui;
 
 import android.app.Activity;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
+
+import com.example.recipesearch.R;
 
 public class SearchResultsActivity extends Activity {
 
@@ -12,7 +18,7 @@ public class SearchResultsActivity extends Activity {
         super.onCreate(savedInstanceState);
         handleIntent(getIntent());
         // Get the intent, verify the action and get the query
-        Intent intent = null;//getIntent(); //null for now
+        Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction()))
         {
             String query = intent.getStringExtra(SearchManager.QUERY);
@@ -38,4 +44,23 @@ public class SearchResultsActivity extends Activity {
         String result = "Food" ; // temp val, will return the result from a search
         return result;
     }
+    public boolean onSearchRequested() {
+        Bundle appData = new Bundle();
+        appData.putBoolean(SearchResultsActivity.INPUT_SERVICE, true);
+        startSearch(null, false, appData, false);
+        return true;
+    }
+  /*  public boolean onCreateOptionsMenu(Menu menu) { // only for use after the settings menu is made 
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.settings_menu).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
+    }*/
 }
