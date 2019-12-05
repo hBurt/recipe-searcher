3eccc56fa550d45f3a8b37401d6998495db4c142
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2019-12-05 16:58:47 -0500

    had this stuff in the wrong file

--- app/src/main/java/com/example/recipesearch/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/recipesearch/ui/search/SearchFragment.java
@@ -16,6 +16,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.recipesearch.R;
+import com.example.recipesearch.ui.SearchResultsActivity;
 
 public class SearchFragment extends Fragment {
 
@@ -24,31 +25,20 @@ public class SearchFragment extends Fragment {
     {
         searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
-        super.onCreate(savedInstanceState);
-        // Get the intent, verify the action and get the query
-        Intent intent = null;//getIntent(); //null for now
-        if (Intent.ACTION_SEARCH.equals(intent.getAction()))
-        {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            preformsearch(query); //will be used to well preform the search
-        }
+
+
         View root = inflater.inflate(R.layout.fragment_search, container, false);
         final TextView textView = root.findViewById(R.id.Search_Result);
         final SearchView search = root.findViewById(R.id.Search_Query);
         searchViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-
                 textView.setText(s);
             }
         });
         return root;
     }
 
-    private String preformsearch(String query) 
-    {
-        String result = "Food" ; // temp val, will return the result from a search
-        return result;
-    }
+
 
 }
