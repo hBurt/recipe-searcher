4f8dae8b0b782aabedd21297dd0dee21a21d46c1
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2019-12-06 14:47:49 -0500

    created things fpr the search bar

---
+++ app/src/main/java/com/example/recipesearch/ui/search/SearchActivity.java
@@ -0,0 +1,90 @@
+package com.example.recipesearch.ui.search;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.example.recipesearch.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity
+{
+    private static String[] Suggestion = new String[]{
+            "Apple","Burger","Apple Pie"
+    };
+    private  MaterialSearchView FsearchView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragmnt_search_results);
+        Toolbar tool = findViewById(R.id.tool);
+        FsearchView = findViewById(R.id.Search_Query);
+        FsearchView .setSuggestions(Suggestion);
+        ListView list = findViewById(R.id.listView);
+        final ArrayAdapter arrayAdapt = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, Suggestion);
+        FsearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown()
+            {
+
+            }
+
+            @Override
+            public void onSearchViewClosed()
+            {
+                arrayAdapt.clear();
+                arrayAdapt.addAll(Suggestion);
+                arrayAdapt.notifyDataSetChanged();
+            }
+        });
+        FsearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                if (arrayAdapt.getCount() > 0)
+                arrayAdapt.clear();
+                if(newText != null && !newText.isEmpty())
+                {
+                    for (String s : Suggestion)
+                    {
+                        if (s.toLowerCase().contains(newText))
+                        {
+                            arrayAdapt.add(s);
+                        }
+                        else
+                            {
+                                arrayAdapt.addAll(Suggestion);
+                            }
+                    }
+
+                }
+                arrayAdapt.notifyDataSetChanged();
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menue_search, menu);
+        MenuItem menuThing = menu.findItem(R.id.searchMenu);
+        FsearchView.setMenuItem(menuThing);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
