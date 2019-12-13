257048c87be96a710d611bbd638d6bf48b3f1057
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2019-12-12 20:47:37 -0500

    just making comments

--- app/src/main/java/com/example/recipesearch/ui/recipe/RecipeActivity.java
+++ app/src/main/java/com/example/recipesearch/ui/recipe/RecipeActivity.java
@@ -7,6 +7,9 @@ import android.widget.Toolbar;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.recipesearch.R;
 import com.google.android.material.tabs.TabItem;
@@ -14,16 +17,18 @@ import com.google.android.material.tabs.TabLayout;
 
 public class RecipeActivity extends AppCompatActivity
 {
-    Recipe_Directions_Tab_Fragment RDTF = new Recipe_Directions_Tab_Fragment();
-    Recipe_Ingredient_Tab_Fragment RITF = new Recipe_Ingredient_Tab_Fragment();
-    Recipe_Similar_Recipes_Tab_Fragment RSRTF = new Recipe_Similar_Recipes_Tab_Fragment();
-    private TextView tex;
+    Recipe_Directions_Tab_Fragment RDTF = new Recipe_Directions_Tab_Fragment(); // case 1
+    Recipe_Ingredient_Tab_Fragment RITF = new Recipe_Ingredient_Tab_Fragment(); // case 0
+    Recipe_Similar_Recipes_Tab_Fragment RSRTF = new Recipe_Similar_Recipes_Tab_Fragment(); //case 2
+    private TextView tex; // all this shit is declared here because it just made it easy incase it was needed outside onCreate
     private ImageView pic;
     private TabLayout tab;
     private TabItem tab1;
     private TabItem tab2;
     private TabItem tab3;
     private String RecipeName = "Test Text";
+    FragmentManager fm = getSupportFragmentManager();
+    FragmentTransaction transaction = fm.beginTransaction();
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -44,26 +49,30 @@ public class RecipeActivity extends AppCompatActivity
             @Override
             public void onTabSelected(TabLayout.Tab tab)
             {
-                switch (tab.getPosition())
+                switch (tab.getPosition())// swaps between the fragments each time a tab is selected
                 {
                     case 0:// ingredient
-
+                        transaction.replace(R.id.Tabs, RDTF);
+                        transaction.commit();
+                        break;
                     case 1: // directions
-
+                        transaction.replace(R.id.Tabs, RSRTF);
+                        transaction.commit();
+                        break;
                     case 2: // similar
-                        
+                        transaction.replace(R.id.Tabs, RITF);
+                        transaction.commit();
+                        break;
                 }
             }
             @Override
             public void onTabUnselected(TabLayout.Tab tab)
-            {
-
+            {//not sure what to do here rn, if anything
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab)
-            {
-
+            {//will do nothing, don't think anything should happen if tabs are reselected
             }
         });
     }
