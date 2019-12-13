3babecaa5edde8508dc9986fb35c14320c503f10
Harrison Steward <HBSteward@student.fullsail.edu>
2019-12-12 19:40:40 -0500

    Added handling for local database

---
+++ app/src/main/java/com/example/recipesearch/database/User.java
@@ -0,0 +1,25 @@
+package com.example.recipesearch.database;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+
+    @ColumnInfo(name = "email")
+    public String email;
+
+    //Use char[], strings are immutable and wont be cleaned up by the garbage collector
+    @ColumnInfo(name = "password")
+    public char[] password;
+}
+
