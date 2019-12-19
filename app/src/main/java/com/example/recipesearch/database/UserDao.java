5fa7e1289ec988db5e322d7292d92f3407e47ece
Harrison Steward <HBSteward@student.fullsail.edu>
2019-12-19 18:34:26 -0500

    Simplified UserDao interface

--- app/src/main/java/com/example/recipesearch/database/UserDao.java
+++ app/src/main/java/com/example/recipesearch/database/UserDao.java
@@ -1,5 +1,6 @@
 package com.example.recipesearch.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -9,26 +10,14 @@ import java.util.List;
 
 @Dao
 public interface UserDao {
-    @Query("SELECT * FROM user")
-    List<User> getAll();
-
-    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
-
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
-
-    @Query("SELECT * FROM user WHERE email LIKE :email LIMIT 1")
-    User findByEmail(String email);
-
-    //@Query("SELECT * FROM user WHERE email LIKE :email")
-    //List<Integer> getFavoritesByEmail(String email);
-
     @Insert
-    void insertAll(User... users);
+    void insertDetails(User data);
 
-    @Delete
-    void delete(User user);
+    @Query("select * from User")
+    LiveData<List<User>> getDetails();
+
+    @Query("delete from User")
+    void deleteAllData();
+
 }
 
