7e1d4c7089a515fbc389b7fb2cb99349b886da24
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2020-01-28 20:13:20 -0500

    further work on the meal planner

---
+++ app/src/main/java/com/example/recipesearch/ui/APIComunication/MealPlanGeneration.java
@@ -0,0 +1,76 @@
+package com.example.recipesearch.ui.APIComunication;
+
+import android.os.AsyncTask;
+
+import com.example.recipesearch.ui.user.MealPlanActivity;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class MealPlanGeneration extends AsyncTask<Void, Void, String>
+{
+
+    @Override
+    protected String doInBackground(Void... voids)
+    {
+
+        String TF = new String(MealPlanActivity.getTimePeriod().trim().replace(" ", "%20").replace("&", "%26")
+                .replace(",", "%2c").replace("(", "%28").replace(")", "%29")
+                .replace("!", "%21").replace("=", "%3D").replace("<", "%3C")
+                .replace(">", "%3E").replace("#", "%23").replace("$", "%24")
+                .replace("'", "%27").replace("*", "%2A").replace("-", "%2D")
+                .replace(".", "%2E").replace("/", "%2F").replace(":", "%3A")
+                .replace(";", "%3B").replace("?", "%3F").replace("@", "%40")
+                .replace("[", "%5B").replace("\\", "%5C").replace("]", "%5D")
+                .replace("_", "%5F").replace("`", "%60").replace("{", "%7B")
+                .replace("|", "%7C").replace("}", "%7D"));
+        String Cal = new String(MealPlanActivity.getCaloricNum().trim().replace(" ", "%20").replace("&", "%26")
+                .replace(",", "%2c").replace("(", "%28").replace(")", "%29")
+                .replace("!", "%21").replace("=", "%3D").replace("<", "%3C")
+                .replace(">", "%3E").replace("#", "%23").replace("$", "%24")
+                .replace("'", "%27").replace("*", "%2A").replace("-", "%2D")
+                .replace(".", "%2E").replace("/", "%2F").replace(":", "%3A")
+                .replace(";", "%3B").replace("?", "%3F").replace("@", "%40")
+                .replace("[", "%5B").replace("\\", "%5C").replace("]", "%5D")
+                .replace("_", "%5F").replace("`", "%60").replace("{", "%7B")
+                .replace("|", "%7C").replace("}", "%7D"));
+        String Diet = new String(MealPlanActivity.getDietaryPrefrence().trim().replace(" ", "%20").replace("&", "%26")
+                .replace(",", "%2c").replace("(", "%28").replace(")", "%29")
+                .replace("!", "%21").replace("=", "%3D").replace("<", "%3C")
+                .replace(">", "%3E").replace("#", "%23").replace("$", "%24")
+                .replace("'", "%27").replace("*", "%2A").replace("-", "%2D")
+                .replace(".", "%2E").replace("/", "%2F").replace(":", "%3A")
+                .replace(";", "%3B").replace("?", "%3F").replace("@", "%40")
+                .replace("[", "%5B").replace("\\", "%5C").replace("]", "%5D")
+                .replace("_", "%5F").replace("`", "%60").replace("{", "%7B")
+                .replace("|", "%7C").replace("}", "%7D"));
+        String Exclu = new String(MealPlanActivity.getExclusions().trim().replace(" ", "%20").replace("&", "%26")
+                .replace(",", "%2c").replace("(", "%28").replace(")", "%29")
+                .replace("!", "%21").replace("=", "%3D").replace("<", "%3C")
+                .replace(">", "%3E").replace("#", "%23").replace("$", "%24")
+                .replace("'", "%27").replace("*", "%2A").replace("-", "%2D")
+                .replace(".", "%2E").replace("/", "%2F").replace(":", "%3A")
+                .replace(";", "%3B").replace("?", "%3F").replace("@", "%40")
+                .replace("[", "%5B").replace("\\", "%5C").replace("]", "%5D")
+                .replace("_", "%5F").replace("`", "%60").replace("{", "%7B")
+                .replace("|", "%7C").replace("}", "%7D"));
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame="+TF+"&targetCalories="+Cal+"&diet="+Diet+"&exclude="+Exclu)
+                .get()
+                .addHeader("x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "7dba1c8b6dmsh8c3919fbe127d43p122d00jsn89f1b32d2216")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/example/recipesearch/ui/user/MealPlanActivity.java
+++ app/src/main/java/com/example/recipesearch/ui/user/MealPlanActivity.java
@@ -3,16 +3,88 @@ package com.example.recipesearch.ui.user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.recipesearch.R;
+import com.example.recipesearch.ui.APIComunication.MealPlanGeneration;
 
 public class MealPlanActivity extends AppCompatActivity
 {
+    static String timePeriod = "";
+    static String dietaryPrefrence = "";
+    static String caloricNum = "";
+    static String exclusions= "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_plan);
+        EditText TOD = findViewById(R.id.TimeOfDay);
+        EditText Cals = findViewById(R.id.CalTarget);
+        EditText DietPref = findViewById(R.id.DietPrefrence); // might be used
+        EditText exception = findViewById(R.id.foodExceptions);
+        Button begin = findViewById(R.id.submit);
+        TOD.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                timePeriod = s.toString();
+            }
+        });
+        Cals.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                caloricNum = s.toString();
+            }
+        });
+        DietPref.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                dietaryPrefrence = s.toString();
+            }
+        });
+        exception.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                exclusions = s.toString();
+            }
+        });
+        begin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MealPlanGeneration gen = new MealPlanGeneration();
+               // gen.execute(); // will generate the meal plan
+            }
+        });
     }
+    public static String getTimePeriod(){return timePeriod;}
+    public static String getDietaryPrefrence() {return dietaryPrefrence;}
+    public static String getCaloricNum() {return caloricNum;}
+    public static String getExclusions() {return exclusions;}
 }
