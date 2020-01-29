5dca283bc854066ecece19932ef3e5d411893ca3
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2020-01-28 19:17:30 -0500

    began creation of the meal plan generator

---
+++ app/src/main/java/com/example/recipesearch/ui/user/MealPlanActivity.java
@@ -0,0 +1,18 @@
+package com.example.recipesearch.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.recipesearch.R;
+
+public class MealPlanActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_plan);
+    }
+}
