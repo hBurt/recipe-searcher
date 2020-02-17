6fb93b9eb097137b6edbe50d0a4311c686a3c7ba
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2020-02-17 12:33:20 -0500

    removed something i created

--- app/src/main/java/com/example/recipesearch/ui/APIComunication/CreateRecipeCard.java
+++ app/src/main/java/com/example/recipesearch/ui/APIComunication/CreateRecipeCard.java
@@ -3,27 +3,16 @@ package com.example.recipesearch.ui.APIComunication;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.provider.Settings;
 
-import com.example.recipesearch.database.User;
 import com.example.recipesearch.ui.CustomRecipes.CustomRecipe;
-import com.example.recipesearch.ui.CustomRecipes.NetClient;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.InputStreamBody;
-
-import java.io.ByteArrayInputStream;
+//import com.spoonacular.client.ApiException;
+//import com.spoonacular.DefaultApi;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-
-import retrofit2.http.Multipart;
 
 public class CreateRecipeCard extends AsyncTask<Void, Void, String>
 {
@@ -38,8 +27,20 @@ public class CreateRecipeCard extends AsyncTask<Void, Void, String>
         byte[] data=bo.toByteArray();
         OkHttpClient client = new OkHttpClient();
         // gathers the data and creates what will be sent in the post
-        NetClient net = new NetClient();
-        net.uploadToServer(CustomRecipe.getCImage());
+        String sendTest = "https://api.spoonacular.com/recipes/visualizeRecipe?title=recipe&image="+bit+"&ingredients=Test\\n%20test\\%20testing&instructions=1%20\\n%202%20\\n%203%20\\n%204&readyInMinutes=5&servings=2&mask=ellipseMask&backgroundImage=none&author=John%20Doe&apiKey=46953957ae604aeba07e605696eef0cc";
+        com.squareup.okhttp.Request request = new Request.Builder()
+                .url(sendTest) // will use only the first result
+                .build();
+
+        Response response = null; // needs an id num or will cause an error
+        String responseData = null;
+        try {
+            response = client.newCall(request).execute(); // provides a val for the first search and preforms it
+            responseData = response.body().string();
+            String test = responseData;
+
+        } catch (IOException e) {
+        }
         return null;
     }
     public static void setRecipeLink(String s){ recipeLink = s;}
